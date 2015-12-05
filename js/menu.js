6561c8cd38de600877725e65602d38a2284c5a00
Goaty89 <charles-yau@live.com.my>
2015-12-05 08:15:31 +0800

    First commit

---
+++ js/menu.js
@@ -0,0 +1,37 @@
+var config = new config();
+
+$(function () {
+	$("a").click(function(e){
+		if(config.previousTab!==void(0)){
+			if(this.id !== config.previousTab.id && config.previousTab.id!==""){
+				$(config.previousTab).removeClass("active");
+			}
+		}
+		$(this).parent().addClass('align_normal');
+		if($(".hdr-logo.active").length>0){
+			$(".hdr-logo.active").removeClass('active');
+			var $self = $(this);
+			setTimeout(function() {
+			  $self.addClass("active");
+			}, 1000); 
+		}else{
+			$(this).addClass("active");
+		}
+		
+		config.previousTab = this;
+	});
+	
+	//nav back to original
+	$(".hdr-logo").click(function(){
+		$(this).addClass("active");
+		setTimeout(function() {
+		  $(".menu-nav").removeClass('align_normal');
+		}, 1000);  
+	});
+});
+	
+function config(){
+	//this.activeTab = "";
+	var previousTab = this.previousTab;
+	return previousTab;
+}	
